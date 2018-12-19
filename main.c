5667e95f118e0d8776cc3f3d5f298f22005087ee
Microtubule <corentin.hanon@gmail.com>
2018-12-19 09:53:05 +0100

    Ajout du calcul de b

--- main.c
+++ main.c
@@ -2,18 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX 100
+
 typedef struct{
     unsigned int cout;
     unsigned int poids;
-}objet;
+}Objet;
 
-typedef objet tab[100];
+typedef Objet tab[MAX];
 
-typedef probleme{
+typedef struct{
     tab t_objets;
     float b;
-}
+}Probleme;
+
+float calculB(tab t, unsigned int n);
 
 int main(int argc, char const *argv[]) {
     printf("Hello THE World");
+}
+
+float calculB(tab t, unsigned int n){
+    float S = 0;
+    for(unsigned int i = 0; i < n ; i++){
+        S += t[i].cout;
+    }
+    srand(time(NULL));
+    unsigned int nb_alea = rand()%3 +1;
+    return 0.25*nb_alea*S;
 }
