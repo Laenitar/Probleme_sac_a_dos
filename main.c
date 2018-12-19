beee9aeab6b7b853475d60929d157c41a13c241d
Microtubule <corentin.hanon@gmail.com>
2018-12-19 09:42:19 +0100

    Ajout de la définition de structure

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+typedef struct{
+    unsigned int cout;
+    unsigned int poids;
+}objet;
+
+typedef objet tab[100];
+
+typedef probleme{
+    tab t_objets;
+    float b;
+}
 
 int main(int argc, char const *argv[]) {
     printf("Hello THE World");
